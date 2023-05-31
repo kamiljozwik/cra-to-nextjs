92b59d64205c20445e40e6a69c5c868b6cc66b88
Kamil Jozwik <kamiljozwikpl@gmail.com>
2023-05-31 10:08:52 +0200

    init CRA with react-router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const title = screen.getByText(/Basic Example/i);
+  expect(title).toBeInTheDocument();
 });
